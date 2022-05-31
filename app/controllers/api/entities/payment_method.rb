3d220c687fd33cadc2b17b27fec5b2bf9ace1e67
Artur Egorov <artur.e@bitzlato.com>
2022-05-31 21:33:58 +0300

    Payment methods endpoint

---
+++ app/controllers/api/entities/payment_method.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Api
+  module Entities
+    class PaymentMethod < Grape::Entity
+      expose(
+        :id,
+        documentation: {
+          type: Integer,
+          desc: 'Unique Method identifier in database.'
+        }
+      )
+
+      expose(
+        :name,
+        documentation: {
+          type: String,
+          desc: 'A name to identify method'
+        }
+      )
+    end
+  end
+end
---
+++ spec/requests/api/payment_methods_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::PaymentMethods, type: :request do
+  fixtures :payment_methods
+
+  let(:response_fields) do
+    %w[
+      id
+      name
+    ]
+  end
+
+  describe 'GET /api/methods' do
+    it 'return array of payment methods' do
+      get '/api/methods'
+
+      expect(json_response.first.keys).to match_array(response_fields)
+      expect(response.status).to eq 200
+    end
+  end
+end
