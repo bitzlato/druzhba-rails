530120f73aba2bf27ea2533f578b8fbd1512fbd7
Artur Egorov <artur.e@bitzlato.com>
2022-06-06 16:20:10 +0300

    S1 stage deploy with capistrano

---
+++ config/deploy/s1.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+set :rails_env, :staging
+set :disallow_pushing, false
+set :application, -> { "druzhba-#{fetch(:stage)}" }
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
+
+set :puma_bind, -> { ["tcp://0.0.0.0:977#{fetch(:stage)[1].to_i || 9}", "unix://#{shared_path}/tmp/sockets/puma.sock"] }
+
+server ENV.fetch('STAGING_SERVER'),
+       user: fetch(:user),
+       port: '22',
+       roles: %w[app db daemons].freeze,
+       ssh_options: { forward_agent: true }
