9e1700098ebff65233f4ab881fcf12c439710b10
Artur Egorov <artur.e@bitzlato.com>
2022-06-21 09:49:11 +0300

    Get user_id and set balance_id in background for creating offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -10,4 +10,14 @@ class Offer < ApplicationRecord
   validates :rate, :min, :max, numericality: true
 
   delegate :arbiter, to: :token
+
+  before_validation :set_balance, on: :create
+
+  private
+
+  def set_balance
+    return if user_id.blank? || token_id.blank?
+
+    self.balance = Balance.find_by(token_id: token_id, user_id: user_id)
+  end
 end
